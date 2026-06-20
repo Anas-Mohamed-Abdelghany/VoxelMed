a03c9e7e97d9cbbdaffd5c1c0fd95466cf6c21b6
sandy-khalil <sandykhalil3322@gmail.com>
2026-06-20 23:29:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ Application entry point.  Run with:
 """
 
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
 from image_viewer import ImageViewer
 
 if __name__ == "__main__":
+    # Prevent OpenCV's Qt plugins from overriding PyQt5 plugins
+    os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH", None)
+    
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
     viewer = ImageViewer()
